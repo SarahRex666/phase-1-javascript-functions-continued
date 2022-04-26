2dda2f620e262447a9178118b6310ef69340c94e
SarahRex <sarahrexmakes@gmail.com>
2022-04-25 22:17:37 -0400

    commit tree 4f8e0635bc2c8c67ab6aecb29f8eee030ac62bdd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// code your solution heref
+function saturdayFun(thing = "roller-skate") {
+  return `This Saturday, I want to ${thing}!`;
+}
+
+function mondayWork(thing = "go to the office") {
+  return `This Monday, I will ${thing}.`;
+}
+
+function wrapAdjective(string) {
+  return function (para = "special") {
+    return `You are ${string}${para}${string}!`;
+  };
+}
